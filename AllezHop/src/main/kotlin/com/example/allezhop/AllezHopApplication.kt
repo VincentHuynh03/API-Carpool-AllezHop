e4fdba32a5b6c0993d1f0f0bcf60e0e84adbf90d
Jérémy Cormier1 <jcormier1@dept-info.crosemont.quebec>
2023-10-31 08:20:26 -0400

    Création de AllezHop dans intellij

---
+++ AllezHop/src/main/kotlin/com/example/allezhop/AllezHopApplication.kt
@@ -0,0 +1,11 @@
+package com.example.allezhop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AllezHopApplication
+
+fun main(args: Array<String>) {
+	runApplication<AllezHopApplication>(*args)
+}
---
+++ AllezHop/src/test/kotlin/com/example/allezhop/AllezHopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.allezhop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AllezHopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
