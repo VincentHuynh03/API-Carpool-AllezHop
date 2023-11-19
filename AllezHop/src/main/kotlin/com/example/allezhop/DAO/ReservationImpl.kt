61d1ff8da304cf79fdc478c411fdcf2b4c1d2c74
VincentHuynh03 <vincentgamer2213@outlook.com>
2023-11-18 20:21:09 -0500

    Ajout des methodes Ajouter et Supprimer dans ReservationImpl

--- AllezHop/src/main/kotlin/com/example/allezhop/DAO/ReservationImpl.kt
+++ AllezHop/src/main/kotlin/com/example/allezhop/DAO/ReservationImpl.kt
@@ -2,22 +2,20 @@ package com.example.allezhop.DAO
 
 import com.example.allezhop.Modèles.Reservation
 import crosemont.tdi.g66.restaurantapirest.DAO.SourceDonnées
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 
 
 
 
 @Repository
-class ReservationImpl():  ReservationDAO {
+class ReservationImpl(val db: JdbcTemplate):  ReservationDAO {
 
 
     override fun chercherTous(): List<Reservation> = SourceDonnées.reservations
 
 
-    override fun supprimer(reservation: Reservation): Reservation? {
-        SourceDonnées.reservations.remove(reservation);
-        return reservation;
-    }
+
 
     override fun chercherParCode(code: String): List<Reservation>? {
         TODO()
@@ -29,12 +27,25 @@ class ReservationImpl():  ReservationDAO {
         TODO("Not yet implemented")
     }
 
+
     override fun ajouter(reservation: Reservation): Reservation? {
-        SourceDonnées.reservations.add(reservation);
-        return reservation;
+        val sql = "INSERT INTO your_reservation_table (code, horodatage, trajet_code, utilisateur_code) VALUES (?, ?, ?, ?)"
+        db.update(
+            sql,
+            reservation.code,
+            reservation.horodatage,
+            reservation.trajet_code,
+            reservation.utilisateur_code
+        )
+        return reservation
     }
 
 
+    override fun supprimer(reservation: Reservation): Reservation? {
+        val sql = "DELETE FROM your_reservation_table WHERE code = ?"
+        db.update(sql, reservation.code)
+        return reservation
+    }
 }
 
 
